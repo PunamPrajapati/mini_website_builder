7bf24274631978977a9def838a5b34893af9eda7
Punam Prajapati <poonamprajapati96.pp@gmail.com>
2025-05-12 16:39:21 +0545

    added user login and register and working on authentication

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'phone_number' => 'required|unique:users',
+            'address' => 'string|nullable'
+        ]);
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone_number' => $request->phone_number,
+            'address' => $request->address ?? null,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'data' => $user,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Login successful',
+            'access_token' => $token,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,36 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WebsiteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function() {
+    return "hello";
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('websites')->group(function () {
-    // Get all websites
-    Route::get('/', [WebsiteController::class, 'getAll'])->name('websites.getAll');
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login'])->name('login');
 
-    // Get a specific website by ID
-    Route::get('/{id}', [WebsiteController::class, 'findById'])->name('websites.findById');
-
-    // Create a new website
-    Route::post('/', [WebsiteController::class, 'create'])->name('websites.create');
-
-    // Update an existing website
-    Route::post('/{id}', [WebsiteController::class, 'update'])->name('websites.update');
-
-    // Delete a website
-    Route::delete('/{id}', [WebsiteController::class, 'delete'])->name('websites.delete');
-});
+// Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('websites')->group(function () {
+        // Get all websites
+        Route::get('/', [WebsiteController::class, 'getAll'])->name('websites.getAll');
+    
+        // Get a specific website by ID
+        Route::get('/{id}', [WebsiteController::class, 'findById'])->name('websites.findById');
+    
+        // Create a new website
+        Route::post('/', [WebsiteController::class, 'create'])->name('websites.create');
+    
+        // Update an existing website
+        Route::post('/{id}', [WebsiteController::class, 'update'])->name('websites.update');
+    
+        // Delete a website
+        Route::delete('/{id}', [WebsiteController::class, 'delete'])->name('websites.delete');
+    });
+// });
